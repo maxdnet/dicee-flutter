b40d496c4b3d5d27dcb70ebf72ccecc9f1206062
Massimiliano Dell’Orco <M.DellOrco@almaviva.it>
2019-06-19 18:26:50 +0200

    Finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,29 +17,49 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  static int getDiceNumber() {
+    return Random().nextInt(6) + 1;
+  }
+
+  int leftDiceNumber = getDiceNumber();
+  int rightDiceNumber = getDiceNumber();
+
+  void updateDiceFace() {
+    setState(() {
+      leftDiceNumber = getDiceNumber();
+      rightDiceNumber = getDiceNumber();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: <Widget>[
-        Expanded(
-          child: FlatButton(
-            child: Image.asset('images/dice1.png'),
-            onPressed: () {
-              print('Left button pressed');
-            },
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+              onPressed: () {
+                updateDiceFace();
+              },
+            ),
           ),
-        ),
-        Expanded(
-          child: FlatButton(
-            child: Image.asset('images/dice2.png'),
-            onPressed: () {
-              print('Right button pressed');
-            },
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+              onPressed: () {
+                updateDiceFace();
+              },
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
